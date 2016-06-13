4f8eb5196c2304460ef736830fa308290e9a2553
Thomas Scheiblauer <tom@sharkbay.at>
2016-06-13 15:03:26 +0200

    for convenience make keep-cursor accept node or event and value or a filter function

--- react_bootstrap/src/quiescent_react_extensions/react_bootstrap/uncontrolled.cljs
+++ react_bootstrap/src/quiescent_react_extensions/react_bootstrap/uncontrolled.cljs
@@ -5,14 +5,11 @@
 
 (def FormControl (factory (uncontrolled-component "FormControl" js/ReactBootstrap.FormControl)))
 
-; deprecatedS
+; deprecated
 (def Input (factory (uncontrolled-component "Input" js/ReactBootstrap.Input)))
 
 ; utility functions
-(defn keep-cursor
-  "Wrap this around your filtered value inside the onChange event to make the cursor
-   stay in its place in uncontrolled inputs."
-  [node value]
+(defn- keep-cursor* [node value]
   (when (not= (.-value node) value)
     (let [old-length (count (.-value node))
           old-index (.-selectionStart node)]
@@ -21,3 +18,13 @@
             (set! (.-selectionStart node)
                   (max 0 (+ (- (count value) old-length) old-index))))))
   value)
+
+(defn keep-cursor [node-or-event value-or-func]
+  "Wrap this around your filtered value or filter function inside
+   the onChange event to make the cursor stay in its place in uncontrolled inputs.
+   The first argument may be React's SyntheticEvent or the target node itself"
+  (let [node (or (.-target node-or-event) node-or-event)
+        value (if (fn? value-or-func)
+                (value-or-func (.-value node))
+                value-or-func)]
+    (keep-cursor* node value)))
